20c91bbf85c59926c8740b465907fd9698278074
Sophie <sophielin14@gmail.com>
2023-10-21 09:59:50 -0700

    ptB q1 standalone

--- lab3/ptB_1.m
+++ lab3/ptB_1.m
@@ -1,9 +1,8 @@
 %420 Lab 3
 %Part B
-%F_vca(x_vca) vs position for different I_coil
-clear all
-clc;
-clear;
+clear all; clc; clear; close all;
+figure
+figure(1)
 
 % VCA manufacture's parameters
 vca_bl = 10.2;    % N/amp
@@ -12,6 +11,8 @@ vca_bl = 10.2;    % N/amp
 lab_current = [0.3 0.5 0.7];
 lab_voltage = [2.2 3.7 5.3];
 
+coil_currents = [0.3 0.5 0.7]; % A
+
 % LVDT paramters from lab2 
 % V = V0 + B0*u, so u = (V - V0) / B0
 lvdt_b0 = 0.196;  % V/mm
@@ -19,10 +20,9 @@ lvdt_v0 = -0.072; % V
 
 plot_markers = ['*', 'o', '+'];
 
+% q1 - F_vca(x_vca) vs position for different I_coil
 % for each coil current
-coil_currents = [0.3 0.5 0.7]; % A
 for i = 1 : length(coil_currents)
-    %fileMatrix = readmatrix("data\B2_0.3A.csv");
     sprintf("data\\B2_%.1fA.csv", coil_currents(i))
     fileMatrix = readmatrix(sprintf("data\\B2_%.1fA.csv", coil_currents(i)));
     
@@ -32,18 +32,14 @@ for i = 1 : length(coil_currents)
     
     % force
     vca_current = fileMatrix(:, 3);
+    vca_current = vca_current - lab_current(i); % compensate for LabView offset
     force = vca_bl * vca_current; % wrong, must be a function of position
     plot(position, force);
-    %plot(position, vca_current, plot_markers(i));
     hold on;
 end
 
-%plot(position(:,1), force(:,1), position(:,2), force(:,2), position(:,3), force(:,3));
-%plot(time(focus_times), rhs, time(focus_times), lhs)
 ylabel('VCA Force (N)')
 xlabel('position (mm)')
 legend('0.3A', '0.5A', '0.7A')
-%legend('RHS - Inertia', 'LHS - Torque Balance')
 title('VCA Force vs position for different coil currents')
-%title(sprintf('Part B, Je = %d kg m^2', Je))
 exportgraphics(gca, 'img/b1_F_vs_position.png')
