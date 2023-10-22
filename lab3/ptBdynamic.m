e03c0c5c2cec0eb534faa89eb22f1f9def218d64
Sophie <sophielin14@gmail.com>
2023-10-22 12:24:21 -0700

    ptB q4 done

--- lab3/ptBdynamic.m
+++ lab3/ptBdynamic.m
@@ -33,7 +33,6 @@ r_avg = [];
 %% q4 impedance as a function of frequency
 % Z = R + jwL = V/I
 
-
 freqs = [1 3 5 7 9 10 30 50 70 90 100 300 500 700 90 1000]; % Hz
 
 % results
@@ -51,28 +50,43 @@ for i = 1 : length(freqs)
     coil_i = fileMatrix(:, 5); % A
     
     % trim data to 1 period so computation doesn't take forever
-    time   = time(1:int32(f_sampl/freqs(i)));
-    coil_v = coil_v(1:int32(f_sampl/freqs(i)));
-    coil_i = coil_i(1:int32(f_sampl/freqs(i)));
-    N = length(time);
+    N = f_sampl/freqs(i);
+    time   = time(1:N);
+    coil_v = coil_v(1:N);
+    coil_i = coil_i(1:N);
 
     % curve fitting to sine function
-    x0 = [1 1 1 1]; % initial guess
     xdata = time.';
     % for voltage
     ydata = coil_v.';
+    x0 = [ydata(1) freqs(i)*2*pi 0 0]; % initial guess
     fun = @(A,xdata) A(1).*sin(A(2).*xdata + A(3)) + A(4);
     A = lsqcurvefit(fun,x0,xdata,ydata);
     Vampl(i)  = A(1);
     Vphase(i) = A(3);
     % for current
     ydata = coil_i.';
+    x0 = [ydata(1) freqs(i)*2*pi 0 0]; % initial guess
     fun = @(A,xdata) A(1).*sin(A(2).*xdata + A(3)) + A(4);
     A = lsqcurvefit(fun,x0,xdata,ydata);
     Iampl(i)  = A(1);
     Iphase(i) = A(3);
     
+    fig = figure(8); subplot(4,4,i);
+    plot(time, coil_i)
+    hold on;
+    plot(time,  A(1).*sin(A(2).*time + A(3)) + A(4), 'r') % curve fit
+
 end
+han=axes(fig,'visible','off'); 
+han.XLabel.Visible='on';
+han.YLabel.Visible='on';
+ylabel(han,'Current (A)');
+xlabel(han, 'time (s)');
+sgtitle('Curve Fits of VCA Current vs Time for all frequencies')
+exportgraphics(gca, 'img/b4_i_curve_fits.png')
+hold off;
+%% q4 impedance bode plots
 
 %Vampl(:,1)
 %Vphase(:,1)
@@ -90,6 +104,7 @@ Zphase = angle(V./I);
 
 figure(6);
 plot(freqs, Zampl.','o')
+%plot(freqs, Zampl.')
 ylabel('impedence (Ohms)')
 xlabel('frequncy (Hz)')
 %legend('VCA Coil Current', sprintf('y = %.2fsin(%.2fx%.2f)+%.2f',A(2),A(2),A(3),A(4)))
@@ -98,8 +113,55 @@ exportgraphics(gca, 'img/b4_Z_mag.png')
 
 figure(7);
 plot(freqs, 2*pi/180*Zphase.','+')
+%plot(freqs, 2*pi/180*Zphase.')
 ylabel('impedence phase (deg)')
 xlabel('frequncy (Hz)')
 %legend('VCA Coil Current', sprintf('y = %.2fsin(%.2fx%.2f)+%.2f',A(2),A(2),A(3),A(4)))
 title('Phase of Coil Impedence vs Frequency')
 exportgraphics(gca, 'img/b4_Z_ph.png')
+
+
+%% plot all voltage curve fits - figure(9
+for i = 1 : length(freqs)
+
+    fileMatrix = readmatrix(sprintf("data\\B3_%d.csv", freqs(i)));
+    time   = fileMatrix(:, 1); % s
+    coil_v = fileMatrix(:, 4); % V
+    coil_i = fileMatrix(:, 5); % A
+    
+    % trim data to 1 period so computation doesn't take forever
+    N = f_sampl/freqs(i);
+    time   = time(1:N);
+    coil_v = coil_v(1:N);
+    coil_i = coil_i(1:N);
+
+    % curve fitting to sine function
+    xdata = time.';
+    % for voltage
+    ydata = coil_v.';
+    x0 = [ydata(1) freqs(i)*2*pi 0 0]; % initial guess
+    fun = @(A,xdata) A(1).*sin(A(2).*xdata + A(3)) + A(4);
+    A = lsqcurvefit(fun,x0,xdata,ydata);
+    Vampl(i)  = A(1);
+    Vphase(i) = A(3);
+    fig = figure(9); subplot(4,4,i);
+    plot(time, coil_v)
+    hold on;
+    plot(time,  A(1).*sin(A(2).*time + A(3)) + A(4), 'r') % curve fit
+
+    % for current
+    ydata = coil_i.';
+    x0 = [ydata(1) freqs(i)*2*pi 0 0]; % initial guess
+    fun = @(A,xdata) A(1).*sin(A(2).*xdata + A(3)) + A(4);
+    A = lsqcurvefit(fun,x0,xdata,ydata);
+    Iampl(i)  = A(1);
+    Iphase(i) = A(3);
+end
+han=axes(fig,'visible','off'); 
+han.XLabel.Visible='on';
+han.YLabel.Visible='on';
+ylabel(han,'Voltage (A)');
+xlabel(han, 'time (s)');
+sgtitle('Curve Fits of VCA Voltages vs Time for all frequencies')
+exportgraphics(gca, 'img/b4_v_curve_fits.png')
+hold off;
