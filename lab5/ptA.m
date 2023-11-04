d17c94504752f93f144de3f6c6222466e057ecd6
Sophie <sophielin14@gmail.com>
2023-11-04 15:17:58 -0700

    q2

--- lab5/ptA.m
+++ lab5/ptA.m
@@ -20,21 +20,30 @@ hold off
 
 figure(2)
 
-plot(position1, force1, 'LineWidth', 2)
+plot(position0, force0, 'LineWidth', 2)
 hold on;
+plot(position1, force1, 'LineWidth', 2)
 
-% Curve Fit
-xdata = position1.';
-ydata = force1.';
-funZ = @(C,xdata) C(1).*ptAcurrents(i).^2./(C(2) + xdata).^2
+% Curve Fit 0
+xdata = position0.';
+ydata = force0.';
+funZ = @(C,xdata) C(1).*ptAcurrents(1).^2./(C(2) + xdata).^2
 x0 = [1 1]; % initial guess
 C = lsqcurvefit(funZ,x0,xdata,ydata);
-plot(position1, C(1).*ptAcurrents(i).^2./(C(2) + position).^2, "--")
+plot(position0, C(1).*ptAcurrents(1).^2./(C(2) + position0).^2, "--")
+
+% Curve Fit 1
+xdata = position1.';
+ydata = force1.';
+funZ = @(C,xdata) C(1).*ptAcurrents(2).^2./(C(2) + xdata).^2
+x0 = [1 1]; % initial guess
+C = lsqcurvefit(funZ,x0,xdata,ydata);
+plot(position1, C(1).*ptAcurrents(2).^2./(C(2) + position1).^2, "--")
 
 ylabel('Force (N)')
 xlabel('Position (mm)')
-legend('measured', sprintf('curve fit, C1 = %.2f, C2 = %.2f', C(1), C(2)))
-%legend('0A', '0.3A', '0A curve fit', sprintf('0.3A curve fit, C1 = %.2f, C2 = %.2f', C(1), C(2)))
+%legend('measured', sprintf('curve fit, C1 = %.2f, C2 = %.2f', C(1), C(2)))
+legend('0A', '0.3A', '0A curve fit', sprintf('0.3A curve fit, C1 = %.2f, C2 = %.2f', C(1), C(2)))
 title('Force vs position using F(x_s_a, I_c_o_i_l) = C_1*I_c_o_i_l^2/(C_2 + x_s_a)^2')
 %title(sprintf('Magnitude of Coil Impedence vs Frequency, L from best fit = %.4fH',L))
 exportgraphics(gca, 'img/a2_curve_fit_0.3A.png')
